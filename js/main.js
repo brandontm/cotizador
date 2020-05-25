bd6a4d6e3c0314d6be8cccf6393f7bcc06ce78f1
brandontm <brandon_btm@outlook.com>
2020-05-24 20:43:59 -0600

    Mover codigo javascript a archivo JS

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const itemsPath = './data/datos.json';
+
+const productsElement = document.querySelector('#products');
+const quotationElement = document.querySelector('#quotation');
+const resultElement = document.querySelector('#result');
+
+fetch(itemsPath)
+    .then(response => response.json())
+    .then(items => {
+        items.forEach(item => {
+            const option = document.createElement('option');
+            option.textContent = item['producto'];
+            productsElement.appendChild(option);
+        });
+    });
+
+quotationElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(quotationElement);
+
+    console.log(formData);
+
+    showQuotationDetails(1, 1);
+});
+
+function showQuotationDetails(idProduct, postalCode) {
+    fetch(itemsPath)
+        .then(response => response.json())
+        .then(items => {
+            const result = document.createElement('h1');
+            result.textContent = 'Ni idea men, unos 500?';
+
+            resultElement.innerHTML = '';
+            resultElement.appendChild(result);
+        });
+    // Item data
+    const name = 'Coyota';
+    const price = 100;      // MXN Currency
+    const volume = 1;       // centimeters squared
+    const weight = 250;     // grams
+
+    const distance = 300;   // kilometers
+}
