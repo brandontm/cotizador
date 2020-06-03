0635132468a65b05c3367edfb034a6caca12c7f1
brandontm <brandon_btm@outlook.com>
2020-06-03 09:13:35 -0600

    Load and modify products from quotation Shows list of products added and allows to modify quantity or remove Function to put products in quotation

--- js/cotizar.js
+++ js/cotizar.js
@@ -3,9 +3,122 @@ let db;
 const API_URL = 'http://localhost:3000/api/v1'; // TODO: Change to production URL
 
 window.onload = function () {
-    const formControl = document.querySelector('#test');
+    this.loadDatabase();
 
-    let request = window.indexedDB.open('cart_db', 1);
+}
+
+function showQuotationProducts() {
+    const transaction = db.transaction('product');
+    const objectStore = transaction.objectStore('product');
+
+    const productList = document.querySelector('#result');
+    productList.innerHTML = ''; // clean product list
+
+    objectStore.openCursor().onsuccess = (event) => {
+        let cursor = event.target.result;
+
+        if (cursor) {
+            const quantity = cursor.value.quantity;
+
+            fetch(`${API_URL}/product/${cursor.value.product_id}`)
+                .then(response => response.json())
+                .then((product) => {
+                    //#region show product card
+                    const div = document.createElement('div');
+                    div.classList.add('col-sm-4', 'col-lg-3', 'py-2');
+
+                    const card = document.createElement('div');
+                    card.classList.add('card', 'mb-4', 'h-100');
+
+                    const img = document.createElement('img');
+                    img.classList.add('card-img-top');
+                    img.src = product.image_url || 'https://cdn.onlinewebfonts.com/svg/img_148071.png';
+
+                    const cardBody = document.createElement('div');
+                    cardBody.classList.add('card-body');
+
+                    const cardTitle = document.createElement('h6');
+                    cardTitle.classList.add('card-text');
+
+                    const cardQuantityDropdown = document.createElement('div');
+                    cardQuantityDropdown.classList.add('dropdown');
+
+                    const cardQuantity = document.createElement('button');
+                    cardQuantity.classList.add('btn', 'btn-secondary', 'dropdown-toggle', 'float-right');
+                    cardQuantity.type = 'button';
+                    cardQuantity.id = 'productQuantityDropdownButton';
+                    cardQuantity.dataset.toggle = 'dropdown';
+                    cardQuantity.ariaHasPopup = 'true';
+                    cardQuantity.ariaExpanded = 'false';
+                    cardQuantity.textContent = quantity;
+
+                    const cardQuantityValues = document.createElement('div');
+                    cardQuantityValues.classList.add('dropdown-menu');
+
+                    cardQuantityValues.labelledBy = 'productQuantityDropdownButton';
+
+                    [...Array(20).keys()].forEach((number) => {
+                        const value = number + 1;
+                        const cardQuantValue = document.createElement('a');
+                        cardQuantValue.classList.add('dropdown-item');
+
+                        cardQuantValue.onclick = () => {
+                            this.putQuotationProduct(product.id, Number(cardQuantValue.textContent));
+                        };
+
+                        if (value === quantity)
+                            cardQuantValue.classList.add('active');
+
+                        cardQuantValue.href = '#';
+                        cardQuantValue.textContent = value;
+
+                        cardQuantityValues.appendChild(cardQuantValue);
+                    });
+
+                    const cardFooter = document.createElement('div');
+                    cardFooter.classList.add('card-footer');
+
+                    const removeProductButton = document.createElement('button');
+                    removeProductButton.classList.add('btn', 'btn-link', 'float-left');
+                    removeProductButton.textContent = 'Eliminar';
+                    removeProductButton.onclick = () => {
+                        this.removeProductFromQuotation(product.id);
+                    };
+
+                    cardQuantityDropdown.appendChild(cardQuantity);
+                    cardQuantityDropdown.appendChild(cardQuantityValues);
+
+                    cardTitle.textContent = `${product.title}`;
+
+                    cardBody.appendChild(cardTitle);
+
+                    cardFooter.appendChild(removeProductButton);
+                    cardFooter.appendChild(cardQuantityDropdown);
+
+                    // card.appendChild(img);
+                    // card.append(document.createElement('hr')); // add separator
+                    card.appendChild(cardBody);
+                    card.appendChild(cardFooter);
+
+                    div.appendChild(card);
+
+                    productList.appendChild(div);
+
+                    console.log(`${product.id} FINISHED`);
+
+
+                    //#endregion
+                });
+
+
+            console.log(`CONTINUE, id ${cursor.value.product_id}`);
+            cursor.continue();
+        }
+    }
+}
+
+function loadDatabase() {
+    const request = window.indexedDB.open('cart_db', 1);
 
     request.onerror = () => {
         // TODO: Error handling
@@ -14,98 +127,72 @@ window.onload = function () {
     request.onsuccess = () => {
         db = request.result;
 
-        showCartProducts();
+        this.showQuotationProducts();
     }
 
     request.onupgradeneeded = (event) => {
         // Get opened database
-        let db = event.target.result;
+        db = event.target.result;
 
         // Create object store (table)
         let objectStore = db.createObjectStore('product', { keyPath: 'product_id' });
         objectStore.createIndex('quantity', 'quantity', { unique: false });
     };
-
-    formControl.onsubmit = (event) => {
-        event.preventDefault();
-
-        // Add item
-        let newItem = { 'product_id': 5, 'quantity': 1 };
-
-        let transaction = db.transaction(['product'], 'readwrite');
-
-        let objectStore = transaction.objectStore('product');
-
-        let request = objectStore.add(newItem);
-
-        request.onsuccess = () => {
-
-        };
-
-        transaction.oncomplete = () => {
-            console.log('Transaction completed: database modification finished.');
-
-            showCartProducts();
-        };
-
-        transaction.onerror = () => {
-            // TODO: Error handling ?
-            console.log('Transaction not opened due to error');
-        };
-    }
 }
 
-function showCartProducts() {
-    let objectStore = db.transaction('product').objectStore('product');
+function putQuotationProduct(productId, productQuantity) {
+    const newItem = { 'product_id': productId, 'quantity': productQuantity };
 
-    const productList = document.querySelector('#result');
-    productList.innerHTML = '';
+    const transaction = db.transaction(['product'], 'readwrite');
 
-    objectStore.openCursor().onsuccess = (event) => {
-        let cursor = event.target.result;
+    const objectStore = transaction.objectStore('product');
 
-        if (cursor) {
-            fetch(`${API_URL}/product/${cursor.value.product_id}`)
-                .then(response => response.json())
-                .then((product) => {
-                    const div = document.createElement('div');
-                    div.classList.add('col-sm-4');
-                    div.classList.add('col-lg-2');
-                    div.classList.add('py-2');
+    const request = objectStore.put(newItem);
 
-                    const card = document.createElement('div');
-                    card.classList.add('card');
-                    card.classList.add('mb-4');
-                    card.classList.add('h-100');
+    request.onsuccess = () => {
 
-                    const img = document.createElement('img');
-                    img.classList.add('card-img-top');
-                    img.src = product.image_url || 'https://cdn.onlinewebfonts.com/svg/img_148071.png';
+    };
 
+    transaction.oncomplete = () => {
+        console.log('Transaction completed: database modification finished.');
 
-                    const cardBody = document.createElement('div');
-                    cardBody.classList.add('card-body');
+        this.showQuotationProducts();
+    };
 
-                    const cardTitle = document.createElement('h6');
-                    cardTitle.classList.add('card-text');
-                    cardTitle.textContent = `${product.title}`;
-
-                    cardBody.appendChild(cardTitle);
-
-                    card.appendChild(img);
-                    card.append(document.createElement('hr')); // add separator
-                    card.appendChild(cardBody);
-
-                    div.appendChild(card);
-
-                    productList.appendChild(div);
-                });
-
-            cursor.continue();
-        }
-    }
+    transaction.onerror = () => {
+        // TODO: Error handling ?
+        console.log('Transaction not opened due to error');
+    };
 }
 
-function addToProductQuote() {
-    let objectStore = db.transaction('product').objectStore('product');
+function removeProductFromQuotation(productId) {
+    const transaction = db.transaction(['product'], 'readwrite');
+
+    const objectStore = transaction.objectStore('product');
+
+    const request = objectStore.delete(productId);
+
+    request.onsuccess = () => {
+
+    };
+
+    transaction.oncomplete = () => {
+        console.log('Transaction completed: database modification finished.');
+
+        this.showQuotationProducts();
+    };
+
+    transaction.onerror = () => {
+        // TODO: Error handling ?
+        console.log('Transaction not opened due to error');
+    };
+}
+
+function getQuotationProduct(productId) {
+    return new Promise((resolve, reject) => {
+        const objectStore = db.transaction('product').objectStore('product');
+        const request = objectStore.get(productId);
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+    });
 }
