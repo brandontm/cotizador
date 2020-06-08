afb807352f77923f114ebd56617e35f72f1d0049
victor juarez <al02855011@tecmilenio.mx>
2020-06-07 19:10:09 -0600

    actualizacion mostrar productos

--- js/menu.js
+++ js/menu.js
@@ -91,63 +91,14 @@ function showProductCard(product) {
     const cardTitle = document.createElement('h6');
     cardTitle.classList.add('card-text');
 
-    const cardQuantityDropdown = document.createElement('div');
-    cardQuantityDropdown.classList.add('dropdown');
-
-    const cardQuantity = document.createElement('button');
-    cardQuantity.classList.add('btn', 'btn-secondary', 'dropdown-toggle', 'float-right');
-    cardQuantity.type = 'button';
-    cardQuantity.id = 'productQuantityDropdownButton';
-    cardQuantity.dataset.toggle = 'dropdown';
-    cardQuantity.ariaHasPopup = 'true';
-    cardQuantity.ariaExpanded = 'false';
-    //cardQuantity.textContent = quantity;
-
-    const cardQuantityValues = document.createElement('div');
-    cardQuantityValues.classList.add('dropdown-menu');
-
-    [...Array(20).keys()].forEach((number) => {
-        const value = number + 1;
-        const cardQuantValue = document.createElement('a');
-        cardQuantValue.classList.add('dropdown-item');
-
-        cardQuantValue.onclick = () => {
-            this.putQuotationProduct(product.id, Number(cardQuantValue.textContent));
-        };
-
-       /* if (value === quantity)
-            cardQuantValue.classList.add('active');*/
-
-        cardQuantValue.href = '#';
-        cardQuantValue.textContent = value;
-
-        cardQuantityValues.appendChild(cardQuantValue);
-    });
-
-    const cardFooter = document.createElement('div');
-    cardFooter.classList.add('card-footer');
-
-    const removeProductButton = document.createElement('button');
-    removeProductButton.classList.add('btn', 'btn-link', 'float-left');
-    removeProductButton.textContent = 'Eliminar';
-    removeProductButton.onclick = () => {
-        this.removeProductFromQuotation(product.id);
-    };
-
-    cardQuantityDropdown.appendChild(cardQuantity);
-    cardQuantityDropdown.appendChild(cardQuantityValues);
-
     cardTitle.textContent = `${product.title}`;
 
     cardBody.appendChild(cardTitle);
 
-    cardFooter.appendChild(removeProductButton);
-    cardFooter.appendChild(cardQuantityDropdown);
 
-    // card.appendChild(img);
-    // card.append(document.createElement('hr')); // add separator
+     card.appendChild(img);
+     card.append(document.createElement('hr')); // add separator
     card.appendChild(cardBody);
-    card.appendChild(cardFooter);
 
     div.appendChild(card);
 
